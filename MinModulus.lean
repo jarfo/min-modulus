5e7767c135683889533435c27816149c91ce7e0d
jadrian <jose.fonollosa@upc.edu>
2026-09-02 03:38:26 +0200

    feat: prune exact profiles from heavy frontier

--- MinModulus.lean
+++ MinModulus.lean
@@ -164,4 +164,5 @@ import MinModulus.G1MinimalSupportSignatureCritical
 import MinModulus.G1ZeroZeroTwoPureStar
 import MinModulus.G1AllZeroPureStar
 import MinModulus.G1ProfileSingletonDescent
+import MinModulus.G1ProfileDescentFrontier
 import MinModulus.G1Counterexample
